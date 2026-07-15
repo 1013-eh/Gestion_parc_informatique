02b305c1cac5adfe2b2eb43c68db0fa1025fc57a
MarwanCE <jakni.marwan@gmail.com>
2026-07-15 11:34:08 +0100

    Added IP check

--- database/seeders/CentreSeeder.php
+++ database/seeders/CentreSeeder.php
@@ -14,7 +14,7 @@ class CentreSeeder extends Seeder
             'nom_centre' => 'Temara Centre',
             'id_region' => 1,
             'matricule' => 1,
-            'adresse_ip' => '192.168.1.1',
+            'adresse_ip' => '127.0.0',
             'dernier_num_ordre' => 0,
             'type_consultation' => 'ADMIN',
         ]);
@@ -24,7 +24,7 @@ class CentreSeeder extends Seeder
             'nom_centre' => 'Casablanca Centre',
             'id_region' => 2,
             'matricule' => 2,
-            'adresse_ip' => '192.168.2.1',
+            'adresse_ip' => '127.0.1',
             'dernier_num_ordre' => 0,
             'type_consultation' => 'PAR_CENTRE',
         ]);
@@ -34,7 +34,7 @@ class CentreSeeder extends Seeder
             'nom_centre' => 'Rabat Agdal',
             'id_region' => 3,
             'matricule' => 3,
-            'adresse_ip' => '192.168.3.1',
+            'adresse_ip' => '172.27.17',
             'dernier_num_ordre' => 0,
             'type_consultation' => 'GLOBAL',
         ]);
