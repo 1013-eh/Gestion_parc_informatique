ff6d01de0c120fd600e2be84351e7ddd1f28cb79
EL OUALI <leelouali@gmail.com>
2026-07-21 15:46:31 +0100

    Replace user count with nbrUsers variable

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
             <h2 class="font-semibold text-xl text-gray-800">
             {{ __('Gestion des utilisateurs') }}
                 <span class="ml-2 px-2 py-1 text-sm bg-green-100 text-green-800 rounded-full">
-                    {{ $users->count() }}
+                    {{ $nbrUsers }}
                 </span>    
             </h2>
             <a href="{{ route('users.create') }}"
