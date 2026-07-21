c31a040c408cd12b0fd65dfe070d233510e3048a
EL OUALI <leelouali@gmail.com>
2026-07-21 15:36:22 +0100

    Replace $materiels->count() with $nbrMateriels

--- resources/views/materiels/materiels.blade.php
+++ resources/views/materiels/materiels.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Matériels') }}
             <span class="ml-2 px-2 py-1 text-sm bg-green-100 text-green-800 rounded-full">
-                {{ $materiels->count() }}
+                {{ $nbrMateriels}}
             </span>
         </h2>
     </x-slot>
