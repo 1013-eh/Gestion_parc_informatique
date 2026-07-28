53c54394122645360fefa7fd75461f6d0f5e002c
1013-eh <leelouali@gmail.com>
2026-07-28 09:59:55 +0100

    Ajout de la confirmation pour les adresses IP dupliquées

---
+++ database/migrations/2026_07_28_084738_remove_unique_from_adresse_ip_in_centres_table.php
@@ -0,0 +1,20 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('centres', function (Blueprint $table) {
+            $table->dropUnique('centres_adresse_ip_unique');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('centres', function (Blueprint $table) {
+            $table->unique('adresse_ip');
+        });
+    }
+};
